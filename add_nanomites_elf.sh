05d2b276ba072b499da4bec0338fd558ab490637
Simone Menezes <simonemenezes2005@gmail.com>
2026-01-20 00:32:12 +0530

    commits

--- add_nanomites_elf.sh
+++ add_nanomites_elf.sh
@@ -7,7 +7,7 @@ fi
 input_elf=$1
 output_elf=$2
 
-python3 linux/src/prepare_packer.py $input_elf	#add encrypted nanomites to ELF
-xxd -i linux/resc/nanomites_dump linux/src/nanomites_dump.h	#transform nanomites_dump into C header file
-xxd -i linux/resc/nanomites_encrypted linux/src/nanomites_encrypted.h	#transform nanomites_encrypted into C header file
-gcc linux/src/packer.c -lssl -lcrypto -o $output_elf		#compile all resources into single executable so it can run everywhere
+python3 src/prepare_packer.py $input_elf	#add encrypted nanomites to ELF
+xxd -i resc/nanomites_dump src/nanomites_dump.h	#transform nanomites_dump into C header file
+xxd -i resc/nanomites_encrypted src/nanomites_encrypted.h	#transform nanomites_encrypted into C header file
+gcc src/packer.c -lssl -lcrypto -o $output_elf		#compile all resources into single executable so it can run everywhere
